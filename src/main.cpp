fc7b206794a192657666ae50f34399b0577d87b7
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2020-10-25 17:15:04 -0300

    process images in parallel

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
+#include <unistd.h>
 #include "ObservatoryBuilder.h"
 #include "ImageQualityFix.h"
+#include <fstream>
+
+ofstream getLogger();
+
+void processImagesInParallel(int camerasQuantity, list<Image> &images);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
+    int camerasQuantity = 2;
     Observatory observatory = ObservatoryBuilder()
             .withImageResolution(Resolution(2, 2))
-            .withCamerasQuantity(1)
+            .withCamerasQuantity(camerasQuantity)
             .build();
 
     list<Image> images = observatory.takeImagesCapture();
+    ofstream log = getLogger();
+    processImagesInParallel(camerasQuantity, images);
 
-    ImageQualityFix().adjust(images.begin().operator->());
-    
+    log.close();
     return 0;
 }
+
+void processImagesInParallel(int camerasQuantity, list<Image> &images) {
+    pid_t pids[camerasQuantity];
+    for (int i = 0; i < camerasQuantity; ++i) {
+        if ((pids[i] = fork()) < 0) {
+            perror("fork");
+            abort();
+        } else if (pids[i] == 0) {
+            cout << "I am a child. My pid is: " << getpid() << " my ppid is: " << getppid() << " \n";
+            auto it = images.begin();
+            advance(it, i);
+            ImageQualityFix().adjust(it.operator->());
+            exit(0);
+        }
+    }
+}
+
+
+ofstream getLogger() {
+    ofstream log;
+    log.open("/Users/jorge.cabrera/workspace/Facultad/75.59-Concurrentes-TP1/log.txt");
+    return log;
+}
+
+
+/**
+ * https://stackoverflow.com/questions/876605/multiple-child-process
+ * */
