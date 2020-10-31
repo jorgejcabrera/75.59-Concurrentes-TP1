028d862108f8ec4bceae9e2a2fa549b3d6ef2092
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2020-10-30 23:26:29 -0300

    add files to work with shared memory

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include "observatory/ObservatoryBuilder.h"
 #include "image/algorithm/ImageQualityFix.h"
 #include <fstream>
+#include "MemoriaCompartida.h"
 
-void printImages(list<Image> list);
+
+void printImages(list<Image> list) {
+    for (auto &it : list) {
+        cout << it.toString();
+    }
+}
 
 ofstream getLogger() {
     ofstream log;
@@ -17,12 +23,18 @@ void adjustImagesInParallel(list<Image> &images) {
     int imageQuantity = images.size();
     pid_t pids[imageQuantity];
     int n = imageQuantity;
+
     for (int i = 0; i < n; ++i) {
         if ((pids[i] = fork()) < 0) {
             perror("fork");
             abort();
         } else if (pids[i] == 0) {
             cout << "I am a child. My pid is: " << getpid() << " my ppid is: " << getppid() << " \n";
+            string archivo("/bin/bash");
+            MemoriaCompartida<int> memoria(archivo, 'A');
+            int result = memoria.read();
+            std::cout << " Hijo : leo el numero " << result << " de la memoria compartida " << std::endl;
+
             auto it = images.begin();
             advance(it, i);
             ImageQualityFix().adjust(it.operator->());
@@ -39,20 +51,12 @@ int main() {
             .build();
 
     list<Image> images = observatory.takeImagesCapture();
+
     adjustImagesInParallel(images);
     Image finalImage = ImageQualityFix().overlap(&images);
+
     return 0;
 }
-
-void printImages(list<Image> list) {
-    for (auto &it : list) {
-        cout << it.toString();
-    }
-}
-
-
-
-
 
 /**
  * https://stackoverflow.com/questions/876605/multiple-child-process
