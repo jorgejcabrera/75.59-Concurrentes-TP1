ebea3915a64606edc86a2807d9572771aecef27b
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2020-11-01 18:44:53 -0300

    main program on a while loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "log/Logger.h"
 
 int main() {
-
     /** Initialing parameters */
     int camerasQuantity = 3;
     string logLevel = "DEBUG";
@@ -17,29 +16,34 @@ int main() {
             .withCamerasQuantity(camerasQuantity)
             .build();
 
-    /** Taking some images */
-    list<Image> images = observatory.takeImagesCapture();
-    Logger::getInstance(logLevel)->log("Initial images value: ", images);
+    int iteration = 0;
+    while (iteration < 5) {
+        Logger::getInstance(logLevel)->log("----------------------Taking images----------------------");
 
-    /** Creating Shared Memory */
-    SharedMemory memory = SharedMemory(images.size());
-    ImageRepository imageRepository = ImageRepository(images.begin()->getSerializedSize());
+        /** Taking some images */
+        list<Image> images = observatory.takeImagesCapture();
+        Logger::getInstance(logLevel)->log("Initial images value: ", images);
 
-    /** Save all images */
-    imageRepository.saveAll(images, memory.getPtrData());
+        /** Creating Shared Memory */
+        SharedMemory memory = SharedMemory(images.size());
+        ImageRepository imageRepository = ImageRepository(images.begin()->getSerializedSize());
 
-    /** Concurrencia */
-    ImageQualityFix().adjustInParallel(images);
-    Logger::getInstance(logLevel)->log("All images were adjusted successfully.");
+        /** Save all images */
+        imageRepository.saveAll(images, memory.getPtrData());
 
-    /** Retrieving adjusted images */
-    list<Image> adjustedImages = imageRepository.findAll(images.size(), memory.getPtrData());
-    Logger::getInstance(logLevel)->log("Adjusted images value: ", adjustedImages);
+        /** Concurrencia */
+        ImageQualityFix().adjustInParallel(images);
+        Logger::getInstance(logLevel)->log("All images were adjusted successfully.");
 
-    /** Final image */
-    Image finalImage = ImageQualityFix::overlap(adjustedImages);
-    Logger::getInstance(logLevel)->log("Final image: " + finalImage.toString());
+        /** Retrieving adjusted images */
+        list<Image> adjustedImages = imageRepository.findAll(images.size(), memory.getPtrData());
+        Logger::getInstance(logLevel)->log("Adjusted images value: ", adjustedImages);
 
-
+        /** Final image */
+        Image finalImage = ImageQualityFix::overlap(adjustedImages);
+        Logger::getInstance(logLevel)->log("Final image: " + finalImage.toString());
+        iteration++;
+    }
+    
     return 0;
 }
